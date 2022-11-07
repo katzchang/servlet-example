214fef134133f06570e1a97e39edf69d185db2d5
Kazunori Otani <katzchang@gmail.com>
2022-11-07 06:29:45 +0000

    clean

--- src/main/java/com/example/MyServlet.java
+++ src/main/java/com/example/MyServlet.java
@@ -5,33 +5,31 @@ import java.io.PrintWriter;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-import org.apache.commons.codec.digest.DigestUtils;
 import io.opentelemetry.api.*;
 import io.opentelemetry.api.trace.*;
 import io.opentelemetry.context.*;
 
 public class MyServlet extends HttpServlet {
-  Tracer tracer = GlobalOpenTelemetry.getTracer("instrumentation-library-name", "1.0.0");
+  Tracer tracer = GlobalOpenTelemetry.getTracer("instrumentation-servlet-session", "0.1.0");
 
+  private Span spanFromSession(HttpSession session, String spanName) {
+    Span span = (Span) session.getAttribute("rootSpan");
+    if (span != null) {
+      Span childSpan = tracer.spanBuilder(spanName).setParent(Context.current().with(span)).startSpan();
+      return childSpan;
+    } else {
+      Span rootSpan = tracer.spanBuilder(spanName).startSpan();
+      session.setAttribute("rootSpan", rootSpan);
+      return rootSpan;
+    }
+  }
+  
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     HttpSession session = req.getSession(true);
     System.out.println("session id: " + session.getId());
-
-    String hexString = DigestUtils.md5Hex(session.getId());
-    System.out.println(hexString);
-
-    Span span = null;
-    if(session.isNew()) {
-      System.out.println("new session");
-      span = tracer.spanBuilder("MyServlet").startSpan();
-      session.setAttribute("rootSpan", span);
-    } else {
-      System.out.println("ongoing session");
-      Span rootSpan = (Span) session.getAttribute("rootSpan");
-      span = tracer.spanBuilder("MyServlet2").setParent(Context.current().with(rootSpan)).startSpan();
-    }
-
+    
+    Span span = spanFromSession(session, "MyServlet");
     span.makeCurrent();
     System.out.println("span: " + span);
  
@@ -44,7 +42,5 @@ public class MyServlet extends HttpServlet {
     } finally {
       span.end();
     }
-
   }
 }
-
